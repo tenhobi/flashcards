e00eb615496d8e03bef6957e33254a68f18cd955
Honza Bittner <git@honzabittner.cz>
2018-02-18 23:31:15 +0100

    Add AngularDart next to Flutter

---
+++ lib/app_component.dart
@@ -0,0 +1,15 @@
+import 'package:angular/angular.dart';
+
+// AngularDart info: https://webdev.dartlang.org/angular
+// Components info: https://webdev.dartlang.org/components
+
+@Component(
+  selector: 'my-app',
+  styleUrls: const ['app_component.css'],
+  templateUrl: 'app_component.html',
+  directives: const [],
+  providers: const [],
+)
+class AppComponent {
+  // Nothing here yet. All logic is in TodoListComponent.
+}
---
+++ web/main.dart
@@ -0,0 +1,7 @@
+import 'package:angular/angular.dart';
+
+import 'package:flashcards/app_component.dart';
+
+void main() {
+  bootstrap(AppComponent);
+}
