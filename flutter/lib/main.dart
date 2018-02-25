b2fc000d4b888db968f44edea140bbd28f883db8
Honza Bittner <git@honzabittner.cz>
2018-02-25 15:15:24 +0100

    Update

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -18,9 +18,7 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
   final String title;
-
   @override
   _MyHomePageState createState() => new _MyHomePageState();
 }
