4c2830744042e47c2dd2397f60c68555b9804f8a
Honza Bittner <git@honzabittner.cz>
2018-03-05 16:38:31 +0100

    Initial flutter UI draft

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:flashcards_flutter/home_page.dart';
 import 'package:flutter/material.dart';
-import 'package:flashcards_logic/flashcards.dart';
 
 void main() => runApp(new MyApp());
 
@@ -11,69 +11,8 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'flashcards'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  void _decrementCounter() {
-    setState(() {
-      _counter--;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.title),
-      ),
-      body: new GestureDetector(
-        onTap: _decrementCounter,
-        child: new Center(
-          child: new Container(
-            //alignment: Alignment.bottomRight,
-            color: new Color.fromARGB(100, 128, 255, 0),
-            child: new Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                new Text(flashcardsMotto
-                    //'Decrease the number by tapping on the green.',
-                    ),
-                new Text(
-                  'Increase the number by pressing the button',
-                ),
-                new Text(
-                  '$_counter',
-                  style: Theme.of(context).textTheme.display1,
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
+      home: new Scaffold(
+        body: new HomePage(),
       ),
     );
   }
