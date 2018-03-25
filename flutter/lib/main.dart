511bb70ae8345c91a3e8568da039a8945c233005
Honza Bittner <git@honzabittner.cz>
2018-03-25 22:31:05 +0200

    WIP Landing screen (#50)
    * Landing screen init
    
    * Finish Flutter, add landing screen to Angular
    
    * temporary fill
    
    * temporary fix

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flashcards_logic/flashcards.dart';
+
+import 'package:flashcards_flutter/src/landing_screen.dart';
+import 'package:flashcards_flutter/src/main_screen.dart';
 
 void main() => runApp(new MyApp());
 
@@ -11,70 +13,7 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'flashcards'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  void _decrementCounter() {
-    setState(() {
-      _counter--;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.title),
-      ),
-      body: new GestureDetector(
-        onTap: _decrementCounter,
-        child: new Center(
-          child: new Container(
-            //alignment: Alignment.bottomRight,
-            color: new Color.fromARGB(100, 128, 255, 0),
-            child: new Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                new Text(flashcardsMotto
-                    //'Decrease the number by tapping on the green.',
-                    ),
-                new Text(
-                  'Increase the number by pressing the button',
-                ),
-                new Text(
-                  '$_counter',
-                  style: Theme.of(context).textTheme.display1,
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ),
+      home: new LandingScreen(nextScreen: new MainScreen()),
     );
   }
 }
---
+++ flutter/lib/src/landing_screen.dart
@@ -0,0 +1,81 @@
+import 'dart:async';
+import 'package:meta/meta.dart';
+import 'package:flutter/material.dart';
+
+class LandingScreen extends StatefulWidget {
+  final Widget nextScreen;
+
+  @override
+  State<StatefulWidget> createState() => new _LandingScreenState();
+
+  LandingScreen({@required this.nextScreen});
+}
+
+// ignore: mixin_inherits_from_not_object
+class _LandingScreenState extends State<LandingScreen> with SingleTickerProviderStateMixin {
+  final Duration duration = new Duration(seconds: 10);
+  final int animationDuration = 4;
+  final double fontSize = 45.0;
+  bool mainScreenFlag = false;
+  AnimationController animation;
+
+  @override
+  void initState() {
+    super.initState();
+    load();
+    animation = new AnimationController(
+      vsync: this,
+      duration: new Duration(seconds: animationDuration),
+      lowerBound: 0.0,
+      upperBound: 1.0,
+    )
+      ..addListener(() {
+        this.setState(() {});
+      })
+      ..forward();
+  }
+
+  @override
+  void dispose() {
+    animation.dispose();
+    super.dispose();
+  }
+
+  void load() async {
+    await new Future.delayed(duration);
+    skip();
+  }
+
+  void skip() {
+    setState(() {
+      mainScreenFlag = true;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (mainScreenFlag) {
+      return widget.nextScreen;
+    }
+
+    return new Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: new GestureDetector(
+        onTap: skip,
+        child: new Container(
+          color: Colors.transparent,
+          alignment: Alignment.center,
+          child: new Text(
+            'flashcards',
+            style: new TextStyle(
+              fontFamily: 'Lobster',
+              fontWeight: FontWeight.normal,
+              fontSize: Curves.elasticOut.transform(animation.value) * fontSize,
+              color: Theme.of(context).buttonColor,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
