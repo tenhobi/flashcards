473abf4933924710661df177aed30ddaadcedb3a
Honza Bittner <git@honzabittner.cz>
2018-04-03 14:14:47 +0200

    Add google sign in example, see console

--- flutter/lib/src/main_screen.dart
+++ flutter/lib/src/main_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flashcards_flutter/src/firebase_flutter_api.dart';
 import 'package:flutter/material.dart';
 
@@ -6,7 +8,28 @@ import 'package:flashcards_flutter/src/custom_drawer.dart';
 
 import 'package:flashcards_common/common.dart';
 
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
 class MainScreen extends StatelessWidget {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn googleSignIn = new GoogleSignIn();
+
+  Future<FirebaseUser> _signIn() async {
+    GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+    GoogleSignInAuthentication gSA = await googleSignInAccount.authentication;
+    FirebaseUser user = await _auth.signInWithGoogle(idToken: gSA.idToken, accessToken: gSA.accessToken);
+
+    print('user name: ${user.displayName}');
+    print(user);
+    return user;
+  }
+
+  void _signOut() {
+    googleSignIn.signOut();
+    print('User signed out.');
+  }
+
   @override
   Widget build(BuildContext context) {
     return new DefaultTabController(
@@ -33,7 +56,26 @@ class MainScreen extends StatelessWidget {
         ),
         body: new TabBarView(
           children: [
-            new CoursesList(CoursesQueryType.active),
+            new Container(
+              child: new ListView(
+                children: <Widget>[
+                  new RaisedButton(
+                    onPressed: () => _signIn().catchError((e) => print(e)),
+                    child: new Text('Sign in'),
+                    color: Colors.green,
+                  ),
+                  new Padding(
+                    padding: const EdgeInsets.all(10.0),
+                  ),
+                  new RaisedButton(
+                    onPressed: _signOut,
+                    child: new Text('Sign out'),
+                    color: Colors.red,
+                  ),
+                ],
+              ),
+            ),
+//            new CoursesList(CoursesQueryType.active),
             new CoursesList(CoursesQueryType.created),
             new CoursesList(CoursesQueryType.popular),
           ],
