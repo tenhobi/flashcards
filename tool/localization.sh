63a02fb406eed70b6902e0ff6d4f9838923c228e
Dan Balarin <dan.balarin@gmail.com>
2018-04-15 17:18:23 +0200

    Fixed tool/localization.sh

--- tool/localization.sh
+++ tool/localization.sh
@@ -2,7 +2,7 @@
 
 function one() {
     cd ../common/
-    pub run intl_translation:generate_from_arb --output-dir=lib/src/i18n/generated --generated-file-prefix=flashcards_ lib/src/i18n/*.dart lib/src/i18n/translations/flashcards_*.arb
+    pub run intl_translation:generate_from_arb --output-dir=lib/src/i18n/generated --generated-file-prefix=flashcards_ lib/src/i18n/*.dart lib/src/i18n/translations/*_*.arb
     echo "Done generating dart files."
     echo "Bye!"
     exit 0
@@ -10,21 +10,23 @@ function one() {
 
 function two() {
     cd ../common/
-    ORIG="../common/lib/src/i18n/intl_messages.arb"
-    COPY="../common/lib/src/i18n/intl_messages_copy.arb"
-    cp $ORIG $COPY 2>/dev/null
-    pub run intl_translation:extract_to_arb --output-dir=lib/src/i18n lib/src/i18n/flashcards_strings.dart
+    ORIG="../common/lib/src/i18n/translations/intl_messages.arb"
+    COPY="../common/lib/src/i18n/translations/template_copy.arb"
+    TEMPLATE="../common/lib/src/i18n/translations/template.arb"
+    cp $TEMPLATE $COPY 2>/dev/null
+    pub run intl_translation:extract_to_arb --output-dir=lib/src/i18n/translations lib/src/i18n/string.dart
+    mv $ORIG $TEMPLATE
+
     echo "Done generating arb file."
     echo "Diff:"
     if ! type "colordiff" > /dev/null; then
-        diff $ORIG $COPY 2>/dev/null
+        diff $TEMPLATE $COPY 2>/dev/null
     else
-        diff $ORIG $COPY | colordiff 2>/dev/null 
+        diff $TEMPLATE $COPY | colordiff 2>/dev/null
     fi
     rm $COPY 2>/dev/null
     echo "Bye!"
     exit 0
-    
 }
 
 echo "Do you want to generate dart files from arb (Method one) or re-generate arb template (Method two)?"
