4d34796f49105d49d4fb4df02e718c1e61c1afd9
Dan Balarin <dan.balarin@gmail.com>
2018-04-16 18:28:25 +0200

    Lint fix

--- common/lib/src/i18n/string.dart
+++ common/lib/src/i18n/string.dart
@@ -3,7 +3,9 @@ import 'package:flashcards_common/src/i18n/generated/messages_all.dart';
 
 void initLocales(List<String> locales) {
   //todo: return loading status and load one locale at time
-  locales.forEach((l) {initializeMessages(l);});
+  locales.forEach((l) {
+    initializeMessages(l);
+  });
 }
 
 class FlashcardsStrings {
