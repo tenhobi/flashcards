30e90dadc8cf870a8d15ca9c4322c3fe41e0ce2f
drabimic <drabinam97@gmail.com>
2018-04-17 16:46:13 +0200

    Added trailing commas

--- flutter/lib/src/about_page.dart
+++ flutter/lib/src/about_page.dart
@@ -6,38 +6,47 @@ import 'package:package_info/package_info.dart';
 class AboutPage extends StatelessWidget {
   String _verNumber = 'TODO VERISON NUMBER';
 
-  final TextStyle _text = TextStyle(color: Colors.white70);
+  final TextStyle _text = TextStyle(
+    color: Colors.white70,
+  );
 
-  final TextStyle _highlight = TextStyle(fontWeight: FontWeight.bold, color: Colors.white);
+  final TextStyle _highlight = TextStyle(
+    fontWeight: FontWeight.bold,
+    color: Colors.white,
+  );
 
   MarkdownStyleSheet _buildMarkdownStyle() {
-    return MarkdownStyleSheet(a: _highlight, p: _text, strong: _highlight, blockSpacing: 30.0);
-  }
-
-  MarkdownBody _buildMarkdown(String data) {
-    return MarkdownBody(
-        data: data,
-        styleSheet: _buildMarkdownStyle(),
-        onTapLink: (link) {
-          launch(link);
-        });
+    return MarkdownStyleSheet(
+      a: _highlight,
+      p: _text,
+      strong: _highlight,
+      blockSpacing: 30.0,
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Theme.of(context).primaryColor,
-        appBar: AppBar(
-          title: Text('About flashcards'),
-          elevation: 0.0,
-        ),
-        body: ListView(padding: EdgeInsets.all(75.0), children: <Widget>[
-          _buildMarkdown('''Version\n__$_verNumber __\n
-Open source project with a goal to develop a multiplatform
-(i.e. web, android, iOS) flashcards learning app. You can check
-our code on [GitHub](https://github.com/tenhobi/flashcards).\n
-The project started as a part of a software team project at
-the [Faculty of Information Technology, CTU in Prague](https://fit.cvut.cz/en).'''),
+      backgroundColor: Theme.of(context).primaryColor,
+      appBar: AppBar(
+        title: Text('About flashcards'),
+        elevation: 0.0,
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(75.0),
+        children: <Widget>[
+          MarkdownBody(
+            data: '''Version
+__$_verNumber __   
+ 
+Open source project with a goal to develop a multiplatform (i.e. web, android, iOS) flashcards learning app. You can check our code on [GitHub](https://github.com/tenhobi/flashcards).
+
+The project started as a part of a software team project at the [Faculty of Information Technology, CTU in Prague](https://fit.cvut.cz/en).''',
+            styleSheet: _buildMarkdownStyle(),
+            onTapLink: (link) {
+              launch(link);
+            },
+          ),
           // Place holder to create indentation
           Text('', style: TextStyle(fontSize: 30.0)),
           // This markdown has to be separated as there is different action needed onTap
@@ -47,6 +56,8 @@ the [Faculty of Information Technology, CTU in Prague](https://fit.cvut.cz/en).'
               onTapLink: (_) {
                 showLicensePage(context: context);
               }),
-        ]));
+        ],
+      ),
+    );
   }
 }
