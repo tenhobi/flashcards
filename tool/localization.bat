4386e4613c221e93d775b0f735b2c27557e5880d
Honza Bittner <git@honzabittner.cz>
2018-05-05 15:01:40 +0200

    Enhance tool scripts, fix NULL in bat

--- tool/localization.bat
+++ tool/localization.bat
@@ -1,18 +1,32 @@
 @ECHO OFF & SETLOCAL EnableDelayedExpansion
 
-ECHO Do you want to generate dart files from arb (Method one) or re-generate arb template (Method two)?
-ECHO 1) Method one
-ECHO 2) Method two
+ECHO Available methods:
+ECHO 1) Generate arb template from source
+ECHO 2) Generate Dart files from arb templates
 ECHO 3) Quit
-CHOICE /C 123 /M "What is your choice"
+CHOICE /C 123 /M "Choose method to use"
 
 IF ERRORLEVEL 3 EXIT /B 0
-IF ERRORLEVEL 2 GOTO TWO
-IF ERRORLEVEL 1 GOTO ONE
+IF ERRORLEVEL 2 GOTO GENERATEDART
+IF ERRORLEVEL 1 GOTO GENERATEARB
 EXIT /B 0
 
-:ONE
-CD..\common
+:GENERATEARB
+CD %~dp0\..\common
+
+SET ORIG=lib\src\i18n\translations\intl_messages.arb
+SET TEMPLATE=lib\src\i18n\translations\template.arb
+
+CALL pub run intl_translation:extract_to_arb --output-dir=lib\src\i18n\translations lib\src\i18n\string.dart
+
+MOVE %ORIG% %TEMPLATE% > NUL
+
+ECHO Done generating arb file.
+ECHO Bye!
+EXIT /B 0
+
+:GENERATEDART
+CD %~dp0\..\common
 
 SET DARTPATH=lib\src\i18n\
 FOR /F "delims=" %%I IN ('DIR /B %DARTPATH%*.dart') DO (
@@ -36,19 +50,5 @@ SET GENERATEDFILES=!GENERATEDFILES! %GENERATEDPATH%%%I
 
 CALL dartfmt --line-length 120 -w !GENERATEDFILES!
 
-ECHO Bye!
-EXIT /B 0
-
-:TWO
-CD..\common
-
-SET ORIG=lib\src\i18n\translations\intl_messages.arb
-SET TEMPLATE=lib\src\i18n\translations\template.arb
-
-CALL pub run intl_translation:extract_to_arb --output-dir=lib\src\i18n\translations lib\src\i18n\string.dart
-
-MOVE %ORIG% %TEMPLATE% > NULL
-
-ECHO Done generating arb file.
 ECHO Bye!
 EXIT /B 0
